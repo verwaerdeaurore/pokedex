f092bbae489babcb7556dd9c59d716dfe007724c
verwaerdeaurore <aurore.verwaerde@ifosup.wavre.be>
2024-01-06 14:11:52 +0100

    responsive

--- pages/pokemon/[slug].vue
+++ pages/pokemon/[slug].vue
@@ -56,22 +56,27 @@ pokemon.value = data.value.pokemon;
 </script>
 
 <template>
-  <div v-if="pokemon" class="w-full m-auto w-2/3 bg-[#f3f3f4] p-5 rounded-lg">
-    <div class="flex flex-row">
-      <div class="w-1/3 m-10">
-        <h2 class="text-6xl text-center tracking-widest mb-10">
+  <div
+    v-if="pokemon"
+    class="mx-auto w-full sm:w-3/4 lg:w-2/3 bg-[#f3f3f4] p-5 rounded-lg"
+  >
+    <div class="flex flex-col lg:flex-row">
+      <div class="m-10 lg:w-1/3">
+        <h2
+          class="text-4xl sm:text-5xl lg:text-6xl text-center tracking-widest mb-10"
+        >
           {{ pokemon.nom }}
         </h2>
         <NuxtImg
           :src="pokemon.image.url"
           :alt="pokemon.nom"
-          class="border-4 border-[#2b4967] rounded-lg transition-all duration-300 ease-in-out hover:scale-110"
+          class="border-4 border-[#2b4967] rounded-lg transition-all duration-300 ease-in-out hover:scale-110 max-w-full h-auto"
         />
       </div>
-      <div class="w-2/3 mt-10">
-        <div class="flex flex-row">
+      <div class="mt-10 lg:mt-0 lg:w-2/3">
+        <div class="flex flex-col sm:flex-row">
           <div
-            class="bg-[#7f9eb6] text-white p-4 rounded-lg w-full flex justify-between"
+            class="bg-[#7f9eb6] text-white p-4 rounded-lg w-full flex flex-col sm:flex-row justify-between"
           >
             <p>{{ pokemon.taille }} centimètres</p>
             <p>{{ pokemon.poids }} kilos</p>
@@ -91,36 +96,33 @@ pokemon.value = data.value.pokemon;
                 <NuxtImg
                   :src="type.image.url"
                   :alt="type.nom"
-                  class="mr-2 w-[30px] h-[30px]"
+                  class="mr-2 w-8 h-8"
                 />
                 {{ type.nom }}
               </p>
             </div>
-            <p>{{ pokemon.pv }} pvs</p>
+            <p>{{ pokemon.pv }} PVs</p>
           </div>
         </div>
         <p class="pt-2 italic">{{ pokemon.description }}</p>
         <div v-if="pokemon.attaques" class="mt-5 border-t-2 border-[#7f9eb6]">
-          <div
-            v-if="pokemon.attaques"
-            class="mt-5 bg-[#7f9eb6] shadow-lg rounded-lg p-4"
-          >
+          <div class="bg-[#7f9eb6] shadow-lg rounded-lg p-4">
             <div
               v-for="attaque in pokemon.attaques"
               :key="attaque.nom"
-              class="mt-2 p-2 bg-white bg-opacity-80 rounded hover:bg-opacity-100 hover:shadow-md transition duration-300 ease-in-out flex justify-between items-start"
+              class="mt-2 p-2 bg-white bg-opacity-80 rounded hover:bg-opacity-100 hover:shadow-md transition duration-300 ease-in-out flex flex-col sm:flex-row justify-between items-center"
             >
-              <div>
-                <span class="font-bold uppercase text-[#2b4967]">{{
-                  attaque.nom
-                }}</span>
-                <span class="ml-3 italic text-gray-600">{{
-                  attaque.description
-                }}</span>
+              <div class="flex-grow">
+                <span class="font-bold uppercase text-[#2b4967]">
+                  {{ attaque.nom }}
+                </span>
+                <span class="ml-3 italic text-gray-600">
+                  {{ attaque.description }}
+                </span>
               </div>
               <span class="text-sm font-semibold text-[#2b4967]">
-                {{ attaque.degat }} PV</span
-              >
+                {{ attaque.degat }} PV
+              </span>
             </div>
           </div>
         </div>
